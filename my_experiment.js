bdb49055bcbe32d620670bcb7fc90c52dd3a3e41
RenaH07 <buhibuhikobuta@hotmail.co.jp>
2025-05-23 00:51:16 +0900

    JS修正７

--- my_experiment.js
+++ my_experiment.js
@@ -285,7 +285,7 @@ jsPsych.init({
     // 各刺激への評価をマージ
     const responses = stimulusTrials.map((stim, idx) => {
       return {
-        stimulus: stim.data?.stimulus_filename || `unknown_${idx}`,
+        stimulus: stim.data?.stimulus_filename?.split('/').pop() || `unknown_${idx}`,
         ...likertResponses[idx]?.response
       };
     });
